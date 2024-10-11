1c4b0ee1fb3bac0c2be242a1f09909954a1515b1
DedliParkurKiller123 <t754631@gmail.com>
2024-10-11 12:59:47 +0300

    Last commit

--- JwtSecurityReact/Frontend/src/App.js
+++ JwtSecurityReact/Frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './component/auth/Register';
 import Login from './component/auth/Login';
-import DemoComponent from './component/hello/DemoComponent';
 
 const App = () => {
     return (
@@ -11,7 +10,6 @@ const App = () => {
                 <Routes>
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/hello" element={<DemoComponent/>} />
                 </Routes>
             </div>
         </Router>
